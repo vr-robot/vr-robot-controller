608ff8e680cc8ae8832d1eca69f8e83ed88caf56
rdeepak2002 <rdeepak2002@gmail.com>
2021-12-04 21:31:27 -0500

    adding web sockets

---
+++ Assets/SocketManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class SocketManager : MonoBehaviour
+{
+    WebSocket ws;
+    public string API_URL = "http://4f4a-68-234-129-29.ngrok.io";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ws = new WebSocket(API_URL);
+        ws.Connect();
+        ws.OnMessage += (sender, e) =>
+        {
+            Debug.Log("Message Received from "+((WebSocket)sender).Url+", Data : "+e.Data);
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ws == null)
+        {
+            return;
+        }
+        
+        ws.Send("Hello");
+    }
+}
